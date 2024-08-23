3dea933189522bc59445ae7306c18c79ed92ed7c
Jonathan Rooke <jdoggus@gmail.com>
2024-08-23 13:49:36 -0400

    Added requirements.txt from pip

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+# Instantiate Flask
 app = Flask(__name__)
+
+# Configure the SQLAlchemy database URI (replace 'db' with the name of your database container)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@db/dbname'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Instantiate SQLAlchemy
+db = SQLAlchemy(app)
 
 # In-memory storage for job applications (you might want to use a database later)
 job_applications = []
@@ -8,6 +17,10 @@ job_applications = []
 @app.route('/')
 def index():
     return render_template('index.html', job_applications=job_applications)
+
+class JobApplication(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(120), nullable=False)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_application():
